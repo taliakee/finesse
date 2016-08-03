a7d9e4ad670a058d7b3b4a4beb4b88ba22f101d1
Makeiyah24 <gwc@Girls-Who-Code-48.local>
2016-08-03 13:00:44 -0400

    featured

--- finessefeaturedactions.js
+++ finessefeaturedactions.js
@@ -1,16 +1,34 @@
 function FeaturedVideos(){
-	var allvideos= ["https://www.youtube.com/embed/od0j4aNDGKM,"];
-	var allvideonames=["Intense Ab Burn Blogilates: Difficulty = o o o o o",
-						"POP Pilates Intense Ab Workout Blogilates: o o o o o",
-						"Carly Rowena’s Gym Routine 6 Pack: o o o o o",
-						"Top 5 Best Ab Workout for Gym: o o o o",
-						"Six Pack Workout for gym: o o o o"]
-	var numbervid= allvideos.length;
-	var random=Math.random(numbervid);
-	var postvid= document.getElementById("Video1");
-	postvid.src="allvideos[random]";
-	var postvidtitle= getElementById("Videotitle1");
-	postvid.src="allvideonames[random]";
+	var allvideos= ["https://www.youtube.com/embed/roPGuZEzeiw?list=PLSCcAGyv98icX4pV3k5uIYtzjaVw12myt",
+					"https://www.youtube.com/embed/od0j4aNDGKM",
+					"https://www.youtube.com/embed/kzYQxZPZtNM",
+					"https://www.youtube.com/embed/3g70e2TP1YI",
+					"https://www.youtube.com/embed/TwCBr0Dqlms",
+					"https://www.youtube.com/embed/695PN9xaEhs",
+					"https://www.youtube.com/embed/lCg_gh_fppI",
+					"https://www.youtube.com/embed/9H9qdpUzu4M",
+					"https://www.youtube.com/embed/F1CohjEGbPc",
+					"https://www.youtube.com/embed/hAGfBjvIRFI",
+					"https://www.youtube.com/embed/XKBbn5GBw0Y",
+					"https://www.youtube.com/embed/yhTxFOVAZtg",
+					"https://www.youtube.com/embed/j6gf8r6e3Kc",
+					"https://www.youtube.com/embed/2gQuLmpK_Ok"]
+
+	var numbervid = allvideos.length;
+	var numberfeatured = 14;
+	var	randomold = [];
+	for (i = 0; i < numberfeatured; i++){
+		var random = Math.floor(Math.random() * numbervid);
+		var postvid = document.getElementById("Video" + i);
+		for (r = 0; r < randomold.length; r++){
+			if (random == randomold[r]){
+				random = Math.floor(Math.random() * numbervid);
+			}
+		}
+		var link = allvideos[random];
+		postvid.src = link;
+		randomold.push(random);
+	}
 }
 
 
