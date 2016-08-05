4d45e5dbcd8144e29a28b720c2e6ed358bc4ffc6
taliakee <taliakee@gmail.com>
2016-08-05 09:44:45 -0400

    allaround3

--- Allaround3.js
+++ Allaround3.js
@@ -2,7 +2,11 @@ function random(){
 	var allvids = ["https://www.youtube.com/embed/TwCBr0Dqlms",
 					"https://www.youtube.com/embed/695PN9xaEhs",
 					"https://www.youtube.com/embed/istOU9nxhm8",
-					"https://www.youtube.com/embed/VO4LDgRoYdQ"] //add new videos HERE
+					"https://www.youtube.com/embed/VO4LDgRoYdQ",
+					"https://www.youtube.com/embed/qOdEVngUs-c?list=PLSCcAGyv98idFLg6YYVXAh9vyJJ_mWne_",
+					"https://www.youtube.com/embed/2FVqMOy7gjU",
+					"https://www.youtube.com/embed/atUc9yhbC8w",
+					"https://www.youtube.com/embed/ULhJjRiqfWc"] //add new videos HERE
 
 	var numbervid = allvids.length; //how many videos
 	var numberfeatured = 4; //number of videos displayed
